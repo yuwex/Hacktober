de810455da598ac9591854e101c0be50f6390bfc
AndrewZhovnirovskii <zhovnirovskii.andrew@gmail.com>
2022-10-24 22:58:07 -0400

    Created empty file gameBoard with some writen notes

---
+++ src/objects/gameBoard.ts
@@ -0,0 +1,15 @@
+// this is to help structure our game and allow for multiple lanes
+
+// create instances of Lanes so that we can have multiple
+// have central things like score and song start/stop in this file
+
+// put the key press needed into each notelane (pass into constructor for a noteLane)
+
+// try scale the Gameboard to size instead of hardcoding values
+
+// will need to modify: gameBoard and noteLanes
+
+// creating an empty file for now
+// for these written notes and to help with potential merge conflicts
+
+// hopefully I'll (Andrew) be able to get some time to work on this
